9b8e7d0d6686355a8efa95f3c86d0e073666381e
victoria-agunbiade <vikkysoft0487@gmail.com>
2023-12-24 14:35:21 +0100

    upload file

---
+++ Module_4/Java_Tasks/Task_18.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Task_18 {
+    public static void main(String[] args) {
+        try {
+            Scanner userInput = new Scanner((System.in));
+            System.out.println("How old are you?");
+            int dob = userInput.nextInt();
+        } catch (Exception ageException){
+            System.out.println("This field accepts numbers only");
+            System.out.println(ageException);
+        }
+    }
+}
+
+//Write a code that asks the user for age and that accepts the Integer value.
+// Handle the error that is encountered when the users try to enter a name instead of age
