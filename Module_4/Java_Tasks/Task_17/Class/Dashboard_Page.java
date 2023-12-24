accda2842910cbf758d06c5a8ab1e8325146b120
victoria-agunbiade <vikkysoft0487@gmail.com>
2023-12-24 14:27:19 +0100

    upload file

---
+++ Module_4/Java_Tasks/Task_17/Class/Dashboard_Page.java
@@ -0,0 +1,13 @@
+package Task_17.Class;
+
+public class Dashboard_Page implements Login_Page{
+    @Override
+    public void correctUsername() {
+
+    }
+
+    @Override
+    public void correctPassword() {
+
+    }
+}
---
+++ Module_4/Java_Tasks/Task_17/Class/Login_Page.java
@@ -0,0 +1,8 @@
+package Task_17.Class;
+
+public interface Login_Page {
+
+    void correctUsername();
+
+    void  correctPassword();
+}
